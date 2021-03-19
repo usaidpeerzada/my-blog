c18de281726df29955901d3ec87ba59a15ddd5b6
usaidpeerzada <usaidpeerzada@gmail.com>
2021-03-20 01:33:57 +0530

    dev.to api

--- src/components/bloglist/BlogList.js
+++ src/components/bloglist/BlogList.js
@@ -1,28 +1,48 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
 import "./BlogList.css";
 
-export default function BlogList({ blog, isDark }) {
+export default function BlogList() {
   function openUrlInNewTab(url) {
     if (url !== undefined) {
       var win = window.open(url, "_blank");
       win.focus();
     }
   }
-
+  const [blogState, setBlogState] = useState([]);
+  useEffect(() => {
+    getDevToPosts();
+  }, []);
+  const getDevToPosts = () => {
+    axios
+      .get("https://dev.to/api/articles?username=usaidpeerzada")
+      .then((res) => {
+        console.log(...res.data);
+        setBlogState([...res.data]);
+      });
+  };
   return (
-    <div
-      className="main-blog-container"
-      onClick={() => openUrlInNewTab(blog.url)}
-    >
-      <div className={"blog-container"}>
-        <a className={"blog-card"} href="#blog">
-          <h3 className={"blog-title"}>{blog.title}</h3>
-          <p className={"small"}>{blog.description}</p>
-          <div className="go-corner">
-            <div className="go-arrow">→</div>
-          </div>
-        </a>
-      </div>
-    </div>
+    <>
+      {blogState &&
+        blogState.map((post) => {
+          return (
+            <div
+              key={post.id}
+              className="main-blog-container"
+              onClick={() => openUrlInNewTab(post.url)}
+            >
+              <div className={"blog-container"}>
+                <a className={"blog-card"} href="#blog">
+                  <h3 className={"blog-title"}>{post.title}</h3>
+                  <p className={"small"}>{post.description}</p>
+                  <div className="go-corner">
+                    <div className="go-arrow">→</div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+    </>
   );
 }
